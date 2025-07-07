d8ff0b27e08867efcd33dd2934132785b2a6f3c6
ggomezji <gabriela.gomez-jimenez@inria.fr>
2025-07-07 14:44:02 +0200

    Tested dataloaders on summy data. WORKS

--- scripts/dataset/dataloaders.py
+++ scripts/dataset/dataloaders.py
@@ -1,45 +1,64 @@
-from diff_benchmark.dataset.read_save_dataset import load_dataset
-from diff_benchmark.dataset.generate_dataset import CustomDataset
-import yaml
 from pathlib import Path
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import train_test_split
+
+import torch
+import yaml
+from sklearn.model_selection import StratifiedKFold, train_test_split
+from torch.utils.data import DataLoader, Subset
+
+from diff_benchmark.dataset.generate_dataset import CustomDataset
+from diff_benchmark.dataset.read_save_dataset import load_dataset
 
 with open("configuration.yaml", "r") as file:
     config = yaml.safe_load(file)
-    
+
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
+# ---------- LOAD DATASET ----------
 X, y, gender = load_dataset(Path(config["results_path"]) / "datasets" / "dataset.h5")
+# ##########
+# import numpy as np
 
-skf = StratifiedKFold(n_splits=config["n_splits"], shuffle=True, random_state=config["random_state"])
-folds = list(skf.split(X, gender)) 
+# num_samples = 1000
+# num_features = 20
+# X = np.random.randn(num_samples, num_features)
+# y = np.random.randint(0, 2, size=num_samples)
+# gender = np.random.randint(0, 2, size=num_samples)
+# ##########
+
+skf = StratifiedKFold(
+    n_splits=config["n_splits"], shuffle=True, random_state=config["random_state"]
+)
+folds = list(skf.split(X, gender))
 
 dataset = CustomDataset(X, y, gender)
 
+
 # --------- BALANCED SPLIT IN TRAIN, VALIDATION AND TEST ---------
 for fold_idx, (trainval_idx, test_idx) in enumerate(folds):
-    print(f"\nFold {fold_idx + 1}/{k_folds}")
+    print(f"\nFold {fold_idx + 1}/{config["n_splits"]}")
 
     # Stratified split of trainval into train and val
     train_idx, val_idx = train_test_split(
         trainval_idx,
         test_size=config["val_size"],
         stratify=gender[trainval_idx],
-        random_state=config["random_state"]
+        random_state=config["random_state"],
     )
 
     # Subsets for this fold
     train_dataset = Subset(dataset, train_idx)
-    val_dataset   = Subset(dataset, val_idx)
-    test_dataset  = Subset(dataset, test_idx)
+    val_dataset = Subset(dataset, val_idx)
+    test_dataset = Subset(dataset, test_idx)
 
     # DataLoaders
-    train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True)
-    val_loader   = DataLoader(val_dataset, batch_size=config["batch_size"], shuffle=False)
-    test_loader  = DataLoader(test_dataset, batch_size=config["batch_size"], shuffle=False)
-    breakpoint()
-    
+    train_loader = DataLoader(
+        train_dataset, batch_size=config["batch_size"], shuffle=True
+    )
+    val_loader = DataLoader(val_dataset, batch_size=config["batch_size"], shuffle=False)
+    test_loader = DataLoader(
+        test_dataset, batch_size=config["batch_size"], shuffle=False
+    )
+
     # ---- Train / Val / Test Model----
     print("Training...")
     for X_batch, y_batch, _ in train_loader:
