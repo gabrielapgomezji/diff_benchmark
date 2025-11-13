6fc5ba89209f03a0aaca321b7f410cd2da836e88
Gabriela Gomez <gabriela.gomez-jimenez@inria.fr>
2025-11-13 11:51:04 +0100

    Pipeline to process the correct data according to the model

---
+++ src/diff_benchmark/utils/data_pipeline.py
@@ -0,0 +1,19 @@
+from diff_benchmark.preprocessing.wrapper_brain_data import (
+    DefaultHcpPipeline,
+    # DefaultWandPipeline,
+    ImageHcpPipeline,
+    LcotEmbedHcpPipeline,
+)
+
+def get_data_pipeline(data_type, config):
+    if data_type == "lcot_embed":
+        print("Using LCOT Embeddings Pipeline")
+        brain_preparator = LcotEmbedHcpPipeline(config)
+    elif data_type == "images":
+        print("Using Image Pipeline")
+        brain_preparator = ImageHcpPipeline(config)
+    elif data_type == "array":
+        print("Using Default Array Pipeline")
+        brain_preparator = DefaultHcpPipeline(config)
+    
+    return brain_preparator
