cf67cb8367459084c3c456e7dd67e796a64590dc
Gabriela Gomez <gabriela.gomez-jimenez@inria.fr>
2025-11-21 09:55:19 +0100

    Added function to read surface labels

--- src/diff_benchmark/dataset/utils_dataset.py
+++ src/diff_benchmark/dataset/utils_dataset.py
@@ -1,4 +1,6 @@
 import numpy as np
+import json
+from pathlib import Path
 
 
 def is_valid_embedding(embeddings):
@@ -7,3 +9,36 @@ def is_valid_embedding(embeddings):
         if not np.isnan(array).all():
             return True
     return False
+
+def load_precomputed_coordinates():
+    """
+    Load precomputed left/right hemisphere vertex coordinate files.
+
+    Expected files:
+        base_dir/coords_L.json
+        base_dir/coords_R.json
+
+    Returns:
+        dict_L, dict_R
+
+    Raises:
+        FileNotFoundError if either file is missing.
+    """
+    coord_dir =  Path(__file__).parent.parent.parent.parent / "aux_files" / "vertex_coordinates"
+
+    file_L = coord_dir / "coords_L.json"
+    file_R = coord_dir / "coords_R.json"
+
+    if not file_L.exists() or not file_R.exists():
+        raise FileNotFoundError(
+            f"Missing coordinate files.\n"
+            f"Expected:\n  {file_L}\n  {file_R}\n"
+            f"Please compute them first. Run the command: run python scripts/get_coordinates.py."
+        )
+
+    with open(file_L, "r") as f:
+        dict_L = json.load(f)
+    with open(file_R, "r") as f:
+        dict_R = json.load(f)
+
+    return dict_L, dict_R
