675ea432ed3aa0202f28fe3a57b846cf6e2e1ea9
clbonet <clementbonet@hotmail.fr>
2025-11-26 13:31:43 +0100

    Test fix k

--- src/diff_benchmark/models/lcot_knn.py
+++ src/diff_benchmark/models/lcot_knn.py
@@ -211,16 +211,18 @@ class kNNLCOT(nn.Module):
 
         d_train = self._compute_dist_matrix(self.train_embeddings, self.train_embeddings)
 
-        knn = KNeighborsClassifier(metric="precomputed")
+        knn = KNeighborsClassifier(n_neightbors=5, metric="precomputed")
 
-        param_grid = {"n_neighbors": np.arange(1, 25)}
-        knn_gscv = GridSearchCV(knn, param_grid, cv=5)
-        knn_gscv.fit(d_train, self.targets)
+        # param_grid = {"n_neighbors": np.arange(1, 25)}
+        # knn_gscv = GridSearchCV(knn, param_grid, cv=5)
+        # knn_gscv.fit(d_train, self.targets)
+        # self.model = knn_gscv
+        # print(f"Best parameters:", knn_gscv.best_params_)
 
-        self.model = knn_gscv
-        print(f"Best parameters:", knn_gscv.best_params_)
-
+        knn.fit(d_train, self.targets)
+        self.model = knn
         
+
     def predict(self, dataloader):
         """
         Predict on new data.
