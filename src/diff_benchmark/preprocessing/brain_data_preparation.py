9c6a565544641a0ccbf5931c924e48deaea3ae0e
Gabriela Gomez <gabriela.gomez-jimenez@inria.fr>
2025-12-19 15:56:49 +0100

    Renamed and _unified for testing before merging

---
+++ src/diff_benchmark/preprocessing/brain_data_preparation.py
@@ -0,0 +1,196 @@
+from pathlib import Path
+from typing import Dict, Union
+
+import bids
+import nibabel as nib
+import numpy as np
+from nibabel.filebasedimages import ImageFileError
+from nilearn import image as nimage
+from tqdm import tqdm
+
+from diff_benchmark.preprocessing.preparation_pipeline_unified import BrainDataPreparationPipeline
+from diff_benchmark.preprocessing.wrapper_utils_brain_data import (
+    average_per_parcel,
+    extract_region_data,
+    extract_selected_labels,
+    project_to_surface,
+    resample_schaefer_onto_fs_lr,
+)
+
+
+class DefaultPipeline(BrainDataPreparationPipeline):
+    """
+    DefaultPipeline is a class that extends the BrainDataPreparationPipeline class to handle
+    the preprocessing of brain data for the CamCAN pipeline.
+    Attributes:
+        base_dir (Path): The directory containing the directory data.
+        results_root (Path): The root directory for storing results.
+        metric (str): The metric to compute (e.g., 'rtop', 'md').
+        schaefer_resampled: Resampled Schaefer atlas onto fs_LR.
+        big_delta (float): The big delta value for diffusion metrics.
+        small_delta (float): The small delta value for diffusion metrics.
+    Methods:
+        verify_subject_files(subject_id: str, metric: str) -> bool:
+            Checks if both hemispheres' .scalar.gii files exist for the given subject and metric.
+        compute_microstructure(subject_id: str):
+            Computes microstructure metrics for the given subject and saves the results.
+        run_analysis():
+            Runs the analysis on the scalar files and computes average data per parcel.
+        extract_features():
+            Placeholder method for extracting features (to be implemented).
+    """
+
+    def __init__(self, dataset_config):
+        super().__init__(dataset_config)
+        self.in_derivatives = self.base_dir / "derivatives"
+        self.results_root = Path(dataset_config.results_dir) / "default"
+
+    def verify_subject_files(self, subject_id: str, metric: str) -> bool:
+        """
+        Check if both hemispheres' .scalar.gii files exist for the given subject and metric.
+        """
+        derivatives_dir = (
+            self.results_root / "derivatives" / f"sub-{subject_id}" / "dwi"
+        )
+        left_file = (
+            derivatives_dir / f"sub-{subject_id}_hemi-L_param-{metric}.scalar.gii"
+        )
+        right_file = (
+            derivatives_dir / f"sub-{subject_id}_hemi-R_param-{metric}.scalar.gii"
+        )
+
+        return left_file.exists() and right_file.exists()
+
+    # def run_analysis(self):  # Doing a test
+    def run_analysis_good(self):
+        """Run the analysis on scalar files and compute average data per parcel."""
+        scalar_files = sorted(
+            self.results_root.glob(
+                f"derivatives/sub-*/dwi/*_hemi-L_param-{self.metric}.scalar.gii"
+            )
+        )
+        for left_file in tqdm(scalar_files, desc="Running analysis"):
+            try:
+                subject_id = left_file.stem.split("_")[0].replace("sub-", "")
+                right_file = left_file.with_name(
+                    left_file.name.replace("hemi-L", "hemi-R")
+                )
+
+                left_data = np.nan_to_num(nib.load(left_file).darrays[0].data).clip(
+                    0, 7
+                )
+                right_data = np.nan_to_num(nib.load(right_file).darrays[0].data).clip(
+                    0, 7
+                )
+
+                avg_data = average_per_parcel(
+                    left_data, right_data, self.schaefer_resampled
+                )
+                self.results[subject_id] = avg_data
+            except (FileNotFoundError, OSError, ValueError, IndexError) as e:
+                print(f"[{subject_id}] Expected error during analysis: {e}")
+
+    # def run_analysis_region(self):
+    def run_analysis(self):
+        scalar_files = sorted(
+            self.results_root.glob(
+                f"derivatives/sub-*/dwi/*_hemi-L_param-{self.metric}.scalar.gii"
+            )
+        )
+        for left_file in tqdm(scalar_files, desc="Running analysis"):
+            try:
+                subject_id = left_file.stem.split("_")[0].replace("sub-", "")
+                right_file = left_file.with_name(
+                    left_file.name.replace("hemi-L", "hemi-R")
+                )
+
+                left_data = np.nan_to_num(nib.load(left_file).darrays[0].data).clip(
+                    0, 7
+                )
+                right_data = np.nan_to_num(nib.load(right_file).darrays[0].data).clip(
+                    0, 7
+                )
+
+                target = self.config["data_preparation"]["region"]
+                avg_data = extract_region_data(
+                    left_data,
+                    right_data,
+                    self.schaefer_resampled,
+                    target_substring=target,
+                    average=False,
+                )
+                self.results[subject_id] = avg_data
+            except (FileNotFoundError, OSError, ValueError, IndexError) as e:
+                print(f"[{subject_id}] Expected error during analysis: {e}")
+
+    def extract_features(self):
+        pass
+
+
+class ImagePipeline(BrainDataPreparationPipeline):
+    """
+    ImagePipeline is a class that extends the BrainDataPreparationPipeline class to handle
+    the preprocessing of brain data for the Human Connectome Project (HCP) pipeline.
+    Attributes:
+        base_dir (Path): The directory containing the directory data.
+        results_root (Path): The root directory for storing results.
+        metric (str): The metric to compute (e.g., 'rtop', 'md').
+        schaefer_resampled: Resampled Schaefer atlas onto fs_LR.
+        big_delta (float): The big delta value for diffusion metrics.
+        small_delta (float): The small delta value for diffusion metrics.
+    Methods:
+        verify_subject_files(subject_id: str, metric: str) -> bool:
+            Checks if both hemispheres' .scalar.gii files exist for the given subject and metric.
+        compute_microstructure(subject_id: str):
+            Computes microstructure metrics for the given subject and saves the results.
+        run_analysis():
+            Runs the analysis on the scalar files and computes average data per parcel.
+        extract_features():
+            Placeholder method for extracting features (to be implemented).
+    """
+    def __init__(self, dataset_config):
+        super().__init__(dataset_config)
+        self.in_derivatives = self.base_dir / "derivatives"
+        self.results_root = Path(dataset_config.results_dir) / "default"
+
+    def verify_subject_files(self, subject_id: str, metric: str) -> bool:
+        """
+        Check if whole brain .nii.gii files exist for the given subject and metric.
+        """
+        derivatives_dir = (
+            self.results_root / "derivatives" / f"sub-{subject_id}" / "dwi"
+        )
+        file = derivatives_dir / f"sub-{subject_id}_param-{metric}_dwimap.nii.gz"
+
+        return file.exists()
+
+    def run_analysis(self):
+        img_files = sorted(
+            self.results_root.glob(
+                f"derivatives/sub-*/dwi/*_param-{self.metric}_dwimap.nii.gz"
+            )
+        )
+        for file in tqdm(img_files, desc="Running analysis"):
+            try:
+                subject_id = file.stem.split("_")[0].replace("sub-", "")
+                self.results[subject_id] = file
+            except (FileNotFoundError, OSError, ValueError, IndexError) as e:
+                print(f"[{subject_id}] Expected error during analysis: {e}")
+
+    def run_analysis_region(self):
+        """Run analysis extracting region data."""
+        img_files = sorted(
+            self.results_root.glob(
+                f"derivatives/sub-*/dwi/*_param-{self.metric}_dwimap.nii.gz"
+            )
+        )
+
+        for file in tqdm(img_files, desc="Running analysis"):
+            try:
+                subject_id = file.stem.split("_")[0].replace("sub-", "")
+                self.results[subject_id] = file
+            except (FileNotFoundError, OSError, ValueError, IndexError) as e:
+                print(f"[{subject_id}] Expected error during analysis: {e}")
+
+    def extract_features(self):
+        pass
---
+++ src/diff_benchmark/preprocessing/preparation_pipeline.py
@@ -0,0 +1,180 @@
+# base_pipeline.py
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from pathlib import Path
+
+import pandas as pd
+from joblib import Parallel, delayed
+from tqdm import tqdm
+
+
+@dataclass
+class ProcessingResult:
+    """
+    A class to store and manage the results of processing subjects in a dataset.
+    Attributes:
+        valid_subjects (list[str]): A list of subject IDs that are considered valid.
+        invalid_subjects (list[str]): A list of subject IDs that are considered invalid.
+    Methods:
+        add_valid(subject_id: str): Adds a subject ID to the list of valid subjects.
+        add_invalid(subject_id: str): Adds a subject ID to the list of invalid subjects.
+    """
+
+    valid_subjects: list[str] = field(default_factory=list)
+    invalid_subjects: list[str] = field(default_factory=list)
+
+    def add_valid(self, subject_id: str):
+        """
+        Adds a valid subject ID to the list of valid subjects.
+        Args:
+            subject_id (str): The ID of the subject to be added as valid.
+        """
+
+        self.valid_subjects.append(subject_id)
+
+    def add_invalid(self, subject_id: str):
+        """
+        Adds a subject ID to the list of invalid subjects.
+        Parameters:
+            subject_id (str): The ID of the subject to be marked as invalid.
+        """
+
+        self.invalid_subjects.append(subject_id)
+
+# DataPreparationBrain
+class BrainDataPreparationPipeline(ABC):
+    """
+    BrainDataPreparationPipeline is an abstract base class for preparing and analyzing brain data.
+    Attributes:
+        config (dict): Configuration settings for data preparation and analysis.
+        results (dict): A dictionary to store results of the analysis.
+    Methods:
+        verify_subject_files(subject_id: str, metric: str) -> bool:
+            Abstract method to verify the existence of required files for a subject.
+        compute_microstructure(subject_id: str):
+            Abstract method to compute microstructure data for a subject.
+        run_analysis():
+            Abstract method to execute the analysis on the prepared data.
+        extract_features():
+            Abstract method to extract features from the analyzed data.
+        export_to_csv(output_path: Path):
+            Exports the results to a CSV file at the specified output path.
+        run_pipeline():
+            Orchestrates the data preparation and analysis pipeline, ensuring all required files exist
+            before running the analysis and exporting results to CSV.
+    """
+
+    def __init__(self, dataset_config: dict):
+        self.dataset_config = dataset_config
+        self.results = {}
+
+    @abstractmethod
+    def verify_raw_files(self, subject_id: str) -> bool:
+        """
+        Verifies the existence of raw files for a given subject ID.
+        Args:
+            subject_id (str): The unique identifier for the subject whose raw files are to be verified.
+        """
+
+    @abstractmethod
+    def verify_subject_files(self, subject_id: str, metric: str) -> bool:
+        """
+        Verifies the existence and validity of subject files for a given subject ID and metric.
+        Args:
+            subject_id (str): The unique identifier for the subject whose files are to be verified.
+            metric (str): The metric type that is being checked for the subject.
+        Returns:
+            bool: True if the subject files are valid and exist, False otherwise.
+        """
+
+    @abstractmethod
+    def compute_microstructure(self, subject_id: str):
+        """
+        Compute the microstructure for a given subject.
+        Parameters:
+            subject_id (str): The unique identifier for the subject whose microstructure is to be computed.
+        """
+
+    @abstractmethod
+    def run_analysis(self):
+        """
+        Executes the analysis process.
+        This method is intended to be overridden in subclasses to implement
+        specific analysis logic. Currently, it is a placeholder and does not
+        perform any operations.
+        """
+
+    @abstractmethod
+    def extract_features(self):
+        """
+        Extract features from the input data.
+        This method is intended to be overridden in subclasses to implement
+        specific feature extraction logic. It currently does not perform
+        any operations and serves as a placeholder.
+        """
+
+    def export_to_csv(self) -> pd.DataFrame:
+        """
+        Exports the results to a CSV file.
+        Parameters:
+            output_path (Path): The file path where the CSV will be saved.
+        Returns:
+            DataFrame: A pandas DataFrame containing the exported results.
+        """
+        if not self.results:
+            raise ValueError("No results to save.")
+        df = pd.DataFrame.from_dict(self.results, orient="index")
+        df.index.name = "subject_id"
+        return df
+
+    def run_pipeline(self, recompute: bool = False) -> pd.DataFrame:
+        """
+        Main orchestration: ensures all required files exist before running analysis.
+        """
+        subject_list = sorted(
+            [
+                p.name
+                for p in Path(self.dataset_config.base_dir).iterdir()
+                if p.is_dir() and p.name.isdigit()
+            ]
+        )
+
+        def process_subject(subject_id):
+            """Processes a single subject by checking for required files"""
+            # if not self.verify_subject_files(
+            if self.verify_raw_files(subject_id):
+                if (
+                    self.verify_subject_files(
+                        subject_id, self.dataset_config.metric_to_compute
+                    )
+                    and recompute
+                ):
+                    print(f"[{subject_id}] Recomputing microstructure.")
+                    self.compute_microstructure(subject_id)
+                else:
+                    print(f"[{subject_id}] Missing files — computing microstructure.")
+                    self.compute_microstructure(subject_id)
+            # else:
+            #     print(f"[{subject_id}] All required files found.")
+
+        Parallel(n_jobs=50)(
+            delayed(process_subject)(subject_id)
+            for subject_id in tqdm(subject_list, desc="Processing subjects")
+        )
+
+        # Once all files are ready, run the analysis
+        print("All required files are ready. Now you can run analysis!")
+        # self.run_analysis()
+        # df = self.export_to_csv()
+        # return df
+
+    def run_microstructure_pipeline(self) -> pd.DataFrame:
+        """
+        Main orchestration: ensures all required files exist before running analysis.
+        """
+        print(
+            "All data should be preprocessed already. Getting microstructure files..."
+        )
+        self.run_analysis()
+        df = self.export_to_csv()
+        return df
