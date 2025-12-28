a34c8fdbff0e33bce517f34b53b68e17ed3b11e7
Gabriela Gomez <gabriela.gomez-jimenez@inria.fr>
2025-12-28 17:46:03 +0100

    Removed unused code. Added linear without pca

--- src/diff_benchmark/models/logistic_regression.py
+++ src/diff_benchmark/models/logistic_regression.py
@@ -10,81 +10,14 @@ from diff_benchmark.models.base import NumpyAbstractModel
 from torch.utils.data import DataLoader
 
 
-class PCALogisticRegressionModel(NumpyAbstractModel):
-    """
-    PCALogisticRegressionModel is a model that combines Principal Component Analysis (PCA)
-    with Logistic Regression for dimensionality reduction and classification.
-    Attributes:
-        n_components (int): The number of principal components to keep.
-        pca (PCA): PCA instance for dimensionality reduction.
-        model (LogisticRegression): Logistic regression model for classification.
-    Methods:
-        _dataloader_to_numpy(dataloader):
-            Converts the data from the dataloader into numpy arrays for features and labels.
-        fit(dataloader):
-            Fits the PCA and logistic regression model on the provided dataloader.
-        predict(dataloader):
-            Transforms the input data using PCA and predicts the class labels using the logistic regression model.
-    """
-
-    data_type = "array"
-    prediction_task = None
-
-    def __init__(self):
-        pipeline = Pipeline(
-            [
-                ("scaler", StandardScaler()),
-                ("pca", PCA()),
-                ("logreg", LogisticRegression(max_iter=1000)),
-            ]
-        )
-
-        # Grid of hyperparameters
-        param_grid = {
-            "pca__n_components": [10, 50, 100],  # number of PCA components to try
-            "logreg__C": [0.01, 0.1, 1, 10, 100],  # inverse regularization strength
-            "logreg__penalty": ["l2"],  # penalty (l1 requires saga solver)
-            "logreg__solver": ["lbfgs"],  # stable solver for l2
-        }
-
-        # Grid search object
-        self.model = GridSearchCV(
-            estimator=pipeline,
-            param_grid=param_grid,
-            scoring="accuracy",  # for binary classification
-            cv=5,
-            n_jobs=-1,
-            verbose=1,
-        )
-
-    def fit(self, dataloader: DataLoader):
-        """Fit PCA and then logistic regression on reduced features.
-        Args:
-            dataloader (DataLoader): A PyTorch DataLoader object containing the training data.
-        """
-        features, targets = self._dataloader_to_numpy(dataloader)
-        features_reshaped = features.reshape(features.shape[0], -1)
-        self.model.fit(features_reshaped, targets.flatten())
-        # print("Best params found:", self.model.best_params_)
-
-    def predict(self, dataloader: DataLoader) -> np.ndarray:
-        """Transform input with PCA and predict with logistic regression.
-        Args:
-            dataloader (DataLoader): A PyTorch DataLoader object containing the input data for prediction.
-        Returns:
-            np.ndarray: Predicted class labels for the input data.
-        """
-        features, _ = self._dataloader_to_numpy(dataloader)
-        features_reshaped = features.reshape(features.shape[0], -1)
-        return self.model.predict(features_reshaped)
-
-
 class PCALinearModel(NumpyAbstractModel):
 
     data_type = "array"
 
     def __init__(self, **kwargs):
+        super().__init__()
         self.prediction_task = kwargs.get("prediction_task", None)
+        self.output_dim = 1 #if self.prediction_task == "regression" else 2
         if self.prediction_task == "classification":
             head = LogisticRegression(max_iter=1000)
             scoring = "accuracy"
@@ -126,36 +59,13 @@ class PCALinearModel(NumpyAbstractModel):
             n_jobs=-1,
             verbose=1,
         )
-
-    def fit(self, dataloader: DataLoader):
-        """Fit PCA and then logistic regression on reduced features.
-        Args:
-            dataloader (DataLoader): A PyTorch DataLoader object containing the training data.
-        """
-        assert self.prediction_task in [
-            "classification",
-            "regression",
-        ], f"prediction_task must be set before calling fit(). Got {self.prediction_task}"
-        features, targets = self._dataloader_to_numpy(dataloader)
-        features_reshaped = features.reshape(features.shape[0], -1)
-        self.model.fit(features_reshaped, targets.flatten())
-        # print("Best params found:", self.model.best_params_)
-
-    def predict(self, dataloader: DataLoader) -> np.ndarray:
-        """Transform input with PCA and predict with logistic regression.
-        Args:
-            dataloader (DataLoader): A PyTorch DataLoader object containing the input data for prediction.
-        Returns:
-            np.ndarray: Predicted class labels for the input data.
-        """
-        features, _ = self._dataloader_to_numpy(dataloader)
-        features_reshaped = features.reshape(features.shape[0], -1)
-        return self.model.predict(features_reshaped)
+    def model(self):
+        return self.model
 
 
-class LogisticRegressionModel(NumpyAbstractModel):
+class LinearModel(NumpyAbstractModel):
     """
-    LogisticRegressionModel is a model that uses Logistic Regression for dimensionality reduction and classification.
+    LinearModel is a model that uses Logistic Regression for dimensionality reduction and classification.
     Attributes:
         n_components (int): The number of principal components to keep.
         pca (PCA): PCA instance for dimensionality reduction.
@@ -170,27 +80,44 @@ class LogisticRegressionModel(NumpyAbstractModel):
     """
 
     data_type = "array"
-    prediction_task = None
 
-    def __init__(self):
-        self.model = LogisticRegression(max_iter=100)
+    def __init__(self, **kwargs):
+        super().__init__()
 
-    def fit(self, dataloader: DataLoader):
-        """Fit logistic regression on reduced features.
-        Args:
-            dataloader (DataLoader): A PyTorch DataLoader object containing the training data.
-        """
-        features, targets = self._dataloader_to_numpy(dataloader)
-        features_reshaped = features.reshape(features.shape[0], -1)
-        self.model.fit(features_reshaped, targets.flatten())
+        self.prediction_task = kwargs.get("prediction_task", None)
+        self.output_dim = 1 #2 if self.prediction_task == "regression" else 1
 
-    def predict(self, dataloader: DataLoader) -> np.ndarray:
-        """predict with logistic regression.
-        Args:
-            dataloader (DataLoader): A PyTorch DataLoader object containing the input data for prediction.
-        Returns:
-            np.ndarray: Predicted class labels for the input data.
-        """
-        features, _ = self._dataloader_to_numpy(dataloader)
-        features_reshaped = features.reshape(features.shape[0], -1)
-        return self.model.predict(features_reshaped).reshape(-1, 1)
+        if self.prediction_task == "classification":
+            head = LogisticRegression(max_iter=1000)
+            scoring = "accuracy"
+            param_grid = {
+                "linear__C": [0.01, 0.1, 1],
+                "linear__solver": ["lbfgs"],
+                "linear__penalty": ["l2"],
+            }
+        else:
+            head = Ridge()
+            scoring = "neg_mean_squared_error"
+            param_grid = {
+                "linear__alpha": [0.01, 0.1, 1],
+            }
+
+        pipeline = Pipeline(
+            [
+                ("scaler", StandardScaler()),
+                ("linear", head),
+            ]
+        )
+
+        self._model = GridSearchCV(
+            estimator=pipeline,
+            param_grid=param_grid,
+            scoring=scoring,
+            cv=5,
+            n_jobs=-1,
+            verbose=1,
+        )
+
+    @property
+    def model(self):
+        return self._model
